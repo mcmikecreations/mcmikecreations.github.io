b9f88a7baa63adf25b1c0a1a3280f623bc3622f2
Mykola Morozov <mykola.morozov@tum.de>
2024-02-14 16:23:53 +0100

    Filled publications, moved social to resume.json.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,7 +28,7 @@
 		TelegramSolid
 	} from '$lib/icons';
 
-	import * as socials from '$lib/data/social.json';
+	import resume from '$lib/data/resume.json';
 
 	$: activeUrl = $page.url.pathname;
 </script>
@@ -56,39 +56,39 @@
 	<div class="mx-auto flex flex-wrap justify-between items-center container overflow-hidden">
 		<FooterCopyright href="/" by="Mykola Morozov" year={new Date().getFullYear()} />
 		<div class="flex flex-wrap -ms-6 md:justify-center mt-0">
-			<FooterIcon href={socials.facebook} class="ms-6 mt-4 md:mt-0">
+			<FooterIcon href={resume.basics.profiles.find(x => x.network === 'TUM')?.url} class="ms-6 mt-4 md:mt-0" aria-label="email solid">
+				<EnvelopeSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+			</FooterIcon>
+			<FooterIcon href={resume.basics.profiles.find(x => x.network === 'GitHub')?.url} class="ms-6 mt-4 md:mt-0">
+				<GithubSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+			</FooterIcon>
+			<FooterIcon href={resume.basics.profiles.find(x => x.network === 'LinkedIn')?.url} class="ms-6 mt-4 md:mt-0">
+				<LinkedinSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+			</FooterIcon>
+			<FooterIcon href={resume.basics.profiles.find(x => x.network === 'Telegram')?.url} class="ms-6 mt-4 md:mt-0">
+				<TelegramSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+			</FooterIcon>
+			<FooterIcon href={resume.basics.profiles.find(x => x.network === 'Google Scholar')?.url} class="ms-6 mt-4 md:mt-0">
+				<GoogleScholarSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+			</FooterIcon>
+			<FooterIcon href={resume.basics.profiles.find(x => x.network === 'ResearchGate')?.url} class="ms-6 mt-4 md:mt-0">
+				<ResearchGateSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+			</FooterIcon>
+			<FooterIcon href={resume.basics.profiles.find(x => x.network === 'Orcid')?.url} class="ms-6 mt-4 md:mt-0">
+				<OrcidSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+			</FooterIcon>
+			<FooterIcon href={resume.basics.profiles.find(x => x.network === 'Mastodon')?.url} class="ms-6 mt-4 md:mt-0">
+				<MastodonSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+			</FooterIcon>
+			<FooterIcon href={resume.basics.profiles.find(x => x.network === 'Facebook')?.url} class="ms-6 mt-4 md:mt-0">
 				<FacebookSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
 			</FooterIcon>
 			<Tooltip>Facebook</Tooltip>
-			<FooterIcon href={socials.github} class="ms-6 mt-4 md:mt-0">
-				<GithubSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-			</FooterIcon>
-			<FooterIcon href={socials.instagram} class="ms-6 mt-4 md:mt-0">
+			<FooterIcon href={resume.basics.profiles.find(x => x.network === 'Instagram')?.url} class="ms-6 mt-4 md:mt-0">
 				<InstagramSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
 			</FooterIcon>
-			<FooterIcon href={socials.linkedin} class="ms-6 mt-4 md:mt-0">
-				<LinkedinSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-			</FooterIcon>
-			<FooterIcon href={socials.twitter} class="ms-6 mt-4 md:mt-0">
+			<FooterIcon href={resume.basics.profiles.find(x => x.network === 'X')?.url} class="ms-6 mt-4 md:mt-0">
 				<XCompanySolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-			</FooterIcon>
-			<FooterIcon href={socials.orcid} class="ms-6 mt-4 md:mt-0">
-				<OrcidSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-			</FooterIcon>
-			<FooterIcon href={socials.mastodon} class="ms-6 mt-4 md:mt-0">
-				<MastodonSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-			</FooterIcon>
-			<FooterIcon href={socials.telegram} class="ms-6 mt-4 md:mt-0">
-				<TelegramSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-			</FooterIcon>
-			<FooterIcon href={socials.scholar} class="ms-6 mt-4 md:mt-0">
-				<GoogleScholarSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-			</FooterIcon>
-			<FooterIcon href={socials.researchgate} class="ms-6 mt-4 md:mt-0">
-				<ResearchGateSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
-			</FooterIcon>
-			<FooterIcon href={socials.researchemail} class="ms-6 mt-4 md:mt-0" aria-label="email solid">
-				<EnvelopeSolid class="w-4 h-4 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
 			</FooterIcon>
 		</div>
 	</div>
