bae69d352f145c1bfb40b3aff21d0e0f27fcec48
Mykola Morozov <mykola.morozov@tum.de>
2024-02-22 16:24:38 +0100

    Added transition animation to fererences.

--- src/routes/home/ReferenceTestimonialSection.svelte
+++ src/routes/home/ReferenceTestimonialSection.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import Quotes from '$lib/icons/Quotes.svelte';
-	import Testimonial from '$lib/components/Testimonial.svelte';
+	import { fly } from 'svelte/transition';
+	import { twMerge } from 'tailwind-merge';
+	import { getContext } from 'svelte';
+	const state = getContext('state');
 
 	export let reference : {
 		"name": string,
@@ -10,17 +13,34 @@
 		"date": string,
 		"reference": string
 	};
+	$: transitionSlideIn = {
+		x: $state.forward ? '100%' : '-100%',
+		opacity: 1,
+		width: '100%',
+		height: '100%',
+		duration: $state.slideDuration
+	};
+	$: transitionSlideOut = {
+		x: $state.forward ? '-100%' : '100%',
+		opacity: 0.9,
+		width: '100%',
+		height: '100%',
+		duration: $state.slideDuration
+	};
 </script>
 
-<Testimonial icon={Quotes} classFigure="max-w-screen-lg px-16 mb-[1.625rem]">
-	<blockquote>
-		<p class="max-h-80 sm:max-h-96 md:max-h-none overflow-y-auto text-base md:text-xl font-medium text-gray-900 dark:text-white">"{reference.reference}"</p>
-	</blockquote>
-	<svelte:fragment slot="footer">
-		<div class="flex items-center mb-6 divide-x-2 divide-gray-300 dark:divide-gray-700">
-			<div class="pe-3 text-base font-medium text-gray-900 dark:text-white">{reference.name}</div>
-			<div class="px-3 text-sm font-light text-gray-600 dark:text-gray-400">{reference.position}</div>
-			<div class="ps-3 text-sm font-light text-gray-600 dark:text-gray-400">{reference.institution}</div>
-		</div>
-	</svelte:fragment>
-</Testimonial>
+{#key reference}
+	<figure class="block !w-full h-full object-cover max-w-screen-lg mx-auto px-16 mb-[1.625rem]" out:fly={transitionSlideOut} in:fly={transitionSlideIn}>
+		<Quotes class="size-12 mx-auto mb-3 text-gray-400 dark:text-gray-600" />
+		<blockquote>
+			<p class="max-h-80 sm:max-h-96 md:max-h-none overflow-y-auto text-base md:text-xl font-medium text-gray-900 dark:text-white">"{reference.reference}"</p>
+		</blockquote>
+		<figcaption class="flex items-center justify-center mt-6 space-x-3">
+			<div class="flex items-center mb-6 divide-x-2 divide-gray-300 dark:divide-gray-700">
+				<div class="pe-3 text-base font-medium text-gray-900 dark:text-white">{reference.name}</div>
+				<div class="px-3 text-sm font-light text-gray-600 dark:text-gray-400">{reference.position}</div>
+				<div class="ps-3 text-sm font-light text-gray-600 dark:text-gray-400">{reference.institution}</div>
+			</div>
+		</figcaption>
+	</figure>
+{/key}
