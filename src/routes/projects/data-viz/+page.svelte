617529bf6e6d881aa492bda91f2d6337e7e2f2b0
Mykola Morozov <mykola.morozov@tum.de>
2024-04-02 23:18:48 +0200

    Added Gindelalmschneid hike.

--- src/routes/projects/data-viz/+page.svelte
+++ src/routes/projects/data-viz/+page.svelte
@@ -12,10 +12,9 @@
 		<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
 			{#each maps as map}
 				<Card href={map.route} img={map.image}>
-					<Heading tag="h3">{map.name}</Heading>
+					<Heading tag="h3" class="break-words">{map.name}</Heading>
 				</Card>
 			{/each}
 		</div>
-
 	</div>
 </main>
