7be3ab99bf3479c88f01713ba4cc6041db3605f6
Mykola Morozov <mykola.morozov@tum.de>
2024-04-29 14:32:37 +0200

    Updated canonical link urls. Added Gmund - Tegernsee hike.

--- src/lib/components/AppTitle.svelte
+++ src/lib/components/AppTitle.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import resume from '$lib/data/resume.json';
+	import { page } from '$app/stores';
 
+	const oldUrl = $page.url;
+	const canonicalUrl = new URL(oldUrl.origin.replace('www.', '') + oldUrl.pathname + oldUrl.search);
 	const titleBase = resume.basics.name;
 	export let title : string | undefined;
 </script>
 
 <svelte:head>
 	<title>{titleBase + (title ? (' | ' + title) : '')}</title>
+	<link rel="canonical" href={canonicalUrl.toString()} />
 </svelte:head>
