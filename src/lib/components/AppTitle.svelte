c06e6d749f0f3ad52c6b2239e2a8feaaaa738138
Mykola Morozov <mykola.morozov@tum.de>
2024-05-02 14:00:45 +0200

    Updated canonical link urls. Updated Kranzberg hike.

--- src/lib/components/AppTitle.svelte
+++ src/lib/components/AppTitle.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import resume from '$lib/data/resume.json';
 	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
 
 	const oldUrl = $page.url;
-	const canonicalUrl = new URL(oldUrl.origin.replace('www.', '') + oldUrl.pathname + oldUrl.search);
+	let canonicalUrl = new URL(oldUrl.origin.replace('www.', '') + oldUrl.pathname);
 	const titleBase = resume.basics.name;
 	export let title : string | undefined;
+
+	onMount(() => {
+		canonicalUrl.search = oldUrl.search;
+	});
 </script>
 
 <svelte:head>
