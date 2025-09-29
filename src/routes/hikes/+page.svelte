1fd6e1adfb6187e3e05cbb776e2d21cc35c3f8e1
Mykola Morozov <mcmikecreations@gmail.com>
2025-09-29 19:30:18 +0200

    Added desktop hikes footsteps.

--- src/routes/hikes/+page.svelte
+++ src/routes/hikes/+page.svelte
@@ -18,7 +18,76 @@
 			const clampedScroll = Math.min(scrollY, maxScroll);
 			text.style.transform = `translateY(${clampedScroll}px)`;
 		});
-	});
+
+        const footstepsContainer = document.getElementById('footsteps-container');
+        function footstepsUpdate() {
+            const stepDistanceY = 60;
+            const stepDistanceX = 30;
+            const scrollY = window.scrollY;
+
+            if (!footstepsContainer) return;
+            const stepCount = Math.floor(scrollY / stepDistanceY);
+            const oldStepCount = footstepsContainer.children.length / 2;
+
+            const stepOffsetY = window.innerHeight * ((stepCount * stepDistanceY + window.innerHeight) / document.documentElement.scrollHeight);
+            const stepOffsetX = 50;
+
+            if (oldStepCount > stepCount) {
+                for (let i = stepCount; i < oldStepCount; ++i) {
+                    footstepsContainer.removeChild(footstepsContainer.lastChild!);
+                }
+            } else if (oldStepCount < stepCount) {
+                for (let i = oldStepCount; i < stepCount; ++i) {
+                    const svgNS = footstepsContainer.getAttribute('xmlns');
+                    {
+                        const footstep = document.createElementNS(svgNS, 'rect') as SVGRectElement;
+
+                        const t = i;
+                        const x = stepDistanceX * Math.sin(t);
+                        const y = t * stepDistanceY; // vertical position
+                        footstep.setAttribute('x', '0');
+                        footstep.setAttribute('y', '0');
+                        footstep.setAttribute('width', '20');
+                        footstep.setAttribute('height', '40');
+                        let transform = '';
+                        const angle = Math.atan(-Math.cos(t) * stepDistanceX / stepDistanceY);
+                        transform += ` translate(${x + stepOffsetX} ${y + stepOffsetY})`;
+                        transform += ` rotate(${180 + angle / Math.PI * 180} 10 20)`;
+                        transform += `scale(-1 1)`;
+                        footstep.setAttribute('transform', transform);
+                        footstep.setAttribute('class', 'footstep');
+
+                        footstepsContainer.appendChild(footstep);
+                    }
+                    {
+                        const footstep = document.createElementNS(svgNS, 'rect') as SVGRectElement;
+
+                        // Curve formula (e.g., sine wave)
+                        const t = i + 0.5;
+                        const x = stepDistanceX * Math.sin(t);
+                        const y = t * stepDistanceY; // vertical position
+                        footstep.setAttribute('x', '0');
+                        footstep.setAttribute('y', '0');
+                        footstep.setAttribute('width', '20');
+                        footstep.setAttribute('height', '40');
+                        let transform = '';
+                        const angle = Math.atan(-Math.cos(t) * stepDistanceX / stepDistanceY);
+                        const offsetDistance = 0;
+                        transform += ` translate(${x + stepOffsetX - offsetDistance * Math.sin(angle)} ${y + stepOffsetY + offsetDistance * Math.cos(angle)})`;
+                        transform += `scale(1 1)`;
+                        transform += ` rotate(${180 + angle / Math.PI * 180} 10 20)`;
+                        footstep.setAttribute('transform', transform);
+                        footstep.setAttribute('class', 'footstep');
+
+                        footstepsContainer.appendChild(footstep);
+                    }
+                }
+            }
+        }
+
+        window.addEventListener('scroll', footstepsUpdate);
+        footstepsUpdate();
+    });
 </script>
 
 <div class="w-full h-[100vh]">
@@ -31,9 +100,20 @@
 		<div class="parallax-layer hero-layer6"></div>
 		<div class="absolute z-10 text-white drop-shadow-xl text-[8rem] left-10 top-0 parallax-text">Hikes</div>
 	</div>
-	<div class="h-[100vh] w-full bg-green-500">
+    <!-- TODO: light/dark button -->
+</div>
+<div class="h-[100vh] w-full bg-green-500">
 
-	</div>
+</div>
+<svg
+        width="auto" height="auto"
+        overflow="visible"
+        id="footsteps-container"
+        xmlns="http://www.w3.org/2000/svg">
+</svg>
+<div></div>
+<div style="height: 3000px;">
+    <h1>Scroll down to see footsteps!</h1>
 </div>
 
 <style>
@@ -86,4 +166,23 @@
 		transform: translateY(0);
 		z-index: 6;
 	}
+
+    #footsteps-container {
+        position: absolute;
+        left: 0;
+        top: 0;
+        pointer-events: none;
+    }
+
+    :global(.footstep) {
+        mask-image: url("/images/hikes/hero/footstep.svg");
+        -webkit-mask-image: url("/images/hikes/hero/footstep.svg");
+        mask-position: center;
+        mask-size: 20px 40px;
+        fill: black;
+    }
+
+    :global(html.dark .footstep) {
+        fill: white;
+    }
 </style>
