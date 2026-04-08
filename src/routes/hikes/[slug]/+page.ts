33a1016d5084c671f0d558f7f6ba4d8ad7726aa4
Mykola Morozov <mykola.morozov@tum.de>
2026-04-08 19:13:02 +0200

    Added the rest of points of interest for hikes.

--- src/routes/hikes/[slug]/+page.ts
+++ src/routes/hikes/[slug]/+page.ts
@@ -116,7 +116,7 @@ export const load: PageLoad = async ({ fetch, params }) => {
 				post: {
 					title: date.title ?? hike.name,
 					description: (date.description ? (date.description + ' ') : '') + hike.description,
-					image: date.image ?? hike.image?.replace('/hikes/', '/hikes/thumb/'),
+					image: date.image?.replace('/hikes/', '/hikes/thumb/') ?? hike.image?.replace('/hikes/', '/hikes/thumb/'),
 					content: marked.lexer(post),
 					headers: headers,
 					time: stats.text,
